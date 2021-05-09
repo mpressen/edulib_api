4bd9077b5cdf712b27ee45f9d0c6fa7aaad6e74a
Maximilien Pressensé <maximilien.pressense@protonmail.com>
2021-05-09 17:38:49 +0200

    Seeds: 'domain' discovery

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.create!(
+  title: "Histoire-Géographie 2021"
+)
+
+article = Article.create!(
+  product: product,
+  title: "Histoire-Géographie 2021 étudiant"
+)
+
+establishment = Establishment.create!
+
+student = Student.create!(
+  firstname: "Jean",
+  lastname: "Dupont",
+  email: "jean.dupont@etudiant.fr"
+)
+
+license = License.create!(
+  article: article,
+  establishment: establishment,
+  student: student,
+  start_date: Date.parse("2021-01-01"),
+  end_date: Date.parse("2021-12-31")
+)
